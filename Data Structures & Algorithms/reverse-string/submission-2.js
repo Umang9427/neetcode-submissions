14f056cad2f225000a14793da6d79fa82866b10f
Umang Bhalani <47608193+Umang9427@users.noreply.github.com>
2026-07-22 16:04:24 +0530

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {character[]} s
+     * @return {void} Do not return anything, modify s in-place instead.
+     */
+    reverseString(s) {
+        var left = 0 
+        var right = s.length - 1
+
+        while (left < right){
+            var temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+
+            left++
+            right--
+        }
+    }
+}
