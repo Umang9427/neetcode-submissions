d11109903a0add0360d4cb0b2e79a93b20533e95
Umang Bhalani <47608193+Umang9427@users.noreply.github.com>
2026-08-04 11:48:45 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findMin(nums) {
+        let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        const mid = Math.floor(left + (right - left) / 2);
+
+        if (nums[mid] > nums[right]) {
+            // Min is in the right unsorted section
+            left = mid + 1;
+        } else {
+            // Min is mid or in the left section
+            right = mid;
+        }
+    }
+
+    // When left === right, we have converged on the minimum element
+    return nums[left];
+    }
+}
